763ab1dd0de1f4b94da9fb3b3eedfec76bc1f6d9
BJTMastermind <bjtmastermind@gmail.com>
2024-02-29 22:37:51 -0500

    Implemented the assemble method for creating chunks.dat files from scratch. Closes #1

--- src/me/bjtmastermind/mcpi_parser/ChunksDatParser.java
+++ src/me/bjtmastermind/mcpi_parser/ChunksDatParser.java
@@ -1,11 +1,17 @@
 package me.bjtmastermind.mcpi_parser;
 
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashMap;
+
+import me.bjtmastermind.mcpi_parser.utils.MultiDimentionalArrayUtils;
+import me.bjtmastermind.mcpi_parser.utils.NumberToLEArray;
 
 public class ChunksDatParser {
 
@@ -92,18 +98,60 @@ public class ChunksDatParser {
                 chunks.put(String.format("%d,%d", chunkX, chunkZ), chunk);
             }
 
+            chunkX++;
+            parsedChunks++;
             if (chunkX % 32 == 0) {
                 chunkX = 0;
                 chunkZ++;
             }
-            chunkX++;
-            parsedChunks++;
         }
         return chunks;
     }
 
-    // TODO: Implement this.
-    // public void assemble() {
+    public void assemble(String filepath, HashMap<String, PiChunk> chunks) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        DataOutputStream rawOutput = new DataOutputStream(baos);
 
-    // }
+        try {
+            // First pass: Insert chunk indexes
+            for (int z = 0, sectorsFromStart = 1; z < 32; z++) {
+                for (int x = 0; x < 32; x++) {
+                    PiChunk chunk = chunks.get(String.format("%d,%d", x, z));
+
+                    if (chunk == null) {
+                        rawOutput.write(NumberToLEArray.toLEInt(0));
+                        continue;
+                    }
+
+                    rawOutput.write(21);
+                    rawOutput.write(NumberToLEArray.toLEInt24(sectorsFromStart));
+                    sectorsFromStart += 21;
+                }
+            }
+
+            // Second pass: Insert chunk data
+            for (int z = 0; z < 32; z++) {
+                for (int x = 0; x < 32; x++) {
+                    PiChunk chunk = chunks.get(String.format("%d,%d", x, z));
+
+                    if (chunk == null) {
+                        continue;
+                    }
+
+                    rawOutput.write(NumberToLEArray.toLEInt(82180)); // Should always be the same if chunk exists.
+                    MultiDimentionalArrayUtils.write(rawOutput, chunk.getBlocks());
+                    MultiDimentionalArrayUtils.write(rawOutput, chunk.getData());
+                    MultiDimentionalArrayUtils.write(rawOutput, chunk.getSkylight());
+                    MultiDimentionalArrayUtils.write(rawOutput, chunk.getBlockLight());
+                    MultiDimentionalArrayUtils.write(rawOutput, chunk.getBiomes());
+                    rawOutput.write(new byte[3836]); // extra bytes padding
+                }
+            }
+
+            File output = new File(filepath);
+            Files.write(output.toPath(), baos.toByteArray());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
