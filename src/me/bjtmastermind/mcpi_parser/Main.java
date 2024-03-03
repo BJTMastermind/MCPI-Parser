f8d2708d459d8f3fa2bc9dc0529dc173a5f383bd
BJTMastermind <bjtmastermind@gmail.com>
2024-03-03 12:16:56 -0500

    Testing new entity methods

--- src/me/bjtmastermind/mcpi_parser/Main.java
+++ src/me/bjtmastermind/mcpi_parser/Main.java
@@ -1,40 +1,32 @@
 package me.bjtmastermind.mcpi_parser;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.HashMap;
 
+import me.bjtmastermind.mcpi_parser.entities.PiSheep;
+import me.bjtmastermind.mcpi_parser.enums.DyeColor;
+import me.bjtmastermind.mcpi_parser.utils.CoordConverter;
 import me.bjtmastermind.nbt.tag.CompoundTag;
-import me.bjtmastermind.nbt.tag.FloatTag;
-import me.bjtmastermind.nbt.tag.IntTag;
 import me.bjtmastermind.nbt.tag.ListTag;
-import me.bjtmastermind.nbt.tag.ShortTag;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        ChunksDatParser chunkParser = new ChunksDatParser();
-        HashMap<String, PiChunk> chunks = chunkParser.parse("/home/bjtmastermind/.var/app/com.thebrokenrail.MCPIReborn/.minecraft-pi/games/com.mojang/minecraftWorlds/world/chunks.dat");
+        // ChunksDatParser chunkParser = new ChunksDatParser();
+        // HashMap<String, PiChunk> chunks = chunkParser.parse("/home/bjtmastermind/.var/app/com.thebrokenrail.MCPIReborn/.minecraft-pi/games/com.mojang/minecraftWorlds/world/chunks.dat");
 
         // System.out.println(chunks+"\n"+chunks.size());
-        chunkParser.assemble("./chunks.dat", chunks);
+        // chunkParser.assemble("./chunks.dat", chunks);
 
-        /*EntitiesDatParser entitiesParser = new EntitiesDatParser();
-        entitiesParser.parse("/home/bjtmastermind/.var/app/com.thebrokenrail.MCPIReborn/.minecraft-pi/games/com.mojang/minecraftWorlds/world/entities_old.dat");
+        EntitiesDatParser entitiesParser = new EntitiesDatParser();
+        entitiesParser.parse("/home/bjtmastermind/.var/app/com.thebrokenrail.MCPIReborn/.minecraft-pi/games/com.mojang/minecraftWorlds/world/entities.dat");
 
-        CompoundTag hackedInEntity = new CompoundTag();
-        hackedInEntity.put("id", new IntTag(13));
-        hackedInEntity.put("Health", new ShortTag((short) 8));
-        ListTag<FloatTag> pos = new ListTag<>();
-        pos.add(64f);
-        pos.add(72f);
-        pos.add(64f);
-        hackedInEntity.put("Pos", pos);
+        float[] real = CoordConverter.asRealCoords(-64f, 8, -64f);
+        PiSheep hackedInEntity = new PiSheep(real[0], real[1], real[2], DyeColor.PINK);
 
         ListTag<CompoundTag> entities = entitiesParser.getEntities();
-        entities.add(hackedInEntity);
+        entities.add(hackedInEntity.toCompoundTag());
 
-        entitiesParser.assemble("/home/bjtmastermind/.var/app/com.thebrokenrail.MCPIReborn/.minecraft-pi/games/com.mojang/minecraftWorlds/world/entities.dat", entities, entitiesParser.getTileEntities());
-        */
+        entitiesParser.assemble("/home/bjtmastermind/.var/app/com.thebrokenrail.MCPIReborn/.minecraft-pi/games/com.mojang/minecraftWorlds/world/entities.dat", entitiesParser.getEntities(), entitiesParser.getTileEntities());
     }
 }
