19650f04158e19fbb5890b1e9672f8980d3fdf10
BJTMastermind <bjtmastermind@gmail.com>
2024-03-03 21:20:07 -0500

    More testing

--- src/me/bjtmastermind/mcpi_parser/Main.java
+++ src/me/bjtmastermind/mcpi_parser/Main.java
@@ -1,13 +1,16 @@
 package me.bjtmastermind.mcpi_parser;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 
+import me.bjtmastermind.mcpi_parser.entities.PiEntity;
 import me.bjtmastermind.mcpi_parser.entities.PiSheep;
 import me.bjtmastermind.mcpi_parser.enums.DyeColor;
+import me.bjtmastermind.mcpi_parser.enums.TileEntityType;
+import me.bjtmastermind.mcpi_parser.tile_entities.PiSign;
 import me.bjtmastermind.mcpi_parser.utils.CoordConverter;
-import me.bjtmastermind.nbt.tag.CompoundTag;
-import me.bjtmastermind.nbt.tag.ListTag;
+import me.bjtmastermind.mcpi_parser.utils.LocateHelper;
 
 public class Main {
 
@@ -24,8 +27,13 @@ public class Main {
         float[] real = CoordConverter.asRealCoords(-64f, 8, -64f);
         PiSheep hackedInEntity = new PiSheep(real[0], real[1], real[2], DyeColor.PINK);
 
-        ListTag<CompoundTag> entities = entitiesParser.getEntities();
-        entities.add(hackedInEntity.toCompoundTag());
+        PiSign sign = (PiSign) LocateHelper.locateTileEntity(entitiesParser.getTileEntities(), TileEntityType.SIGN, 21, 66, 29);
+
+        sign.setText2("This sign was");
+        sign.setText3("modded in!");
+
+        ArrayList<PiEntity> entities = entitiesParser.getEntities();
+        entities.add(hackedInEntity);
 
         entitiesParser.assemble("/home/bjtmastermind/.var/app/com.thebrokenrail.MCPIReborn/.minecraft-pi/games/com.mojang/minecraftWorlds/world/entities.dat", entitiesParser.getEntities(), entitiesParser.getTileEntities());
     }
