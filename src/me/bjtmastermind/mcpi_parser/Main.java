8ddffff8fa9fa11daa069c42da91a1b672a05b91
BJTMastermind <bjtmastermind@gmail.com>
2024-03-07 00:41:22 -0500

    testing stuff

--- src/me/bjtmastermind/mcpi_parser/Main.java
+++ src/me/bjtmastermind/mcpi_parser/Main.java
@@ -2,10 +2,12 @@ package me.bjtmastermind.mcpi_parser;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 
 import me.bjtmastermind.mcpi_parser.entities.PiEntity;
 import me.bjtmastermind.mcpi_parser.entities.PiSheep;
+import me.bjtmastermind.mcpi_parser.enums.BlockType;
 import me.bjtmastermind.mcpi_parser.enums.DyeColor;
 import me.bjtmastermind.mcpi_parser.enums.TileEntityType;
 import me.bjtmastermind.mcpi_parser.tile_entities.PiSign;
@@ -15,26 +17,27 @@ import me.bjtmastermind.mcpi_parser.utils.LocateHelper;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        // ChunksDatParser chunkParser = new ChunksDatParser();
-        // HashMap<String, PiChunk> chunks = chunkParser.parse("/home/bjtmastermind/.var/app/com.thebrokenrail.MCPIReborn/.minecraft-pi/games/com.mojang/minecraftWorlds/world/chunks.dat");
+        ChunksDatParser chunkParser = new ChunksDatParser();
+        HashMap<String, PiChunk> chunks = chunkParser.parse("/home/bjtmastermind/.var/app/com.thebrokenrail.MCPIReborn/.minecraft-pi/games/com.mojang/minecraftWorlds/world/chunks.dat");
 
         // System.out.println(chunks+"\n"+chunks.size());
-        // chunkParser.assemble("./chunks.dat", chunks);
 
-        EntitiesDatParser entitiesParser = new EntitiesDatParser();
-        entitiesParser.parse("/home/bjtmastermind/.var/app/com.thebrokenrail.MCPIReborn/.minecraft-pi/games/com.mojang/minecraftWorlds/world/entities.dat");
+        chunkParser.assemble("./chunks.dat", chunks);
 
-        float[] real = CoordConverter.asRealCoords(-64f, 8, -64f);
-        PiSheep hackedInEntity = new PiSheep(real[0], real[1], real[2], DyeColor.PINK);
+        // EntitiesDatParser entitiesParser = new EntitiesDatParser();
+        // entitiesParser.parse("/home/bjtmastermind/.var/app/com.thebrokenrail.MCPIReborn/.minecraft-pi/games/com.mojang/minecraftWorlds/world/entities.dat");
 
-        PiSign sign = (PiSign) LocateHelper.locateTileEntity(entitiesParser.getTileEntities(), TileEntityType.SIGN, 21, 66, 29);
+        // float[] real = CoordConverter.asRealCoords(-64f, 8, -64f);
+        // PiSheep hackedInEntity = new PiSheep(real[0], real[1], real[2], DyeColor.PINK);
 
-        sign.setText2("This sign was");
-        sign.setText3("modded in!");
+        // PiSign sign = (PiSign) LocateHelper.locateTileEntity(entitiesParser.getTileEntities(), TileEntityType.SIGN, 21, 66, 29);
 
-        ArrayList<PiEntity> entities = entitiesParser.getEntities();
-        entities.add(hackedInEntity);
+        // sign.setText2("This sign was");
+        // sign.setText3("modded in!");
 
-        entitiesParser.assemble("/home/bjtmastermind/.var/app/com.thebrokenrail.MCPIReborn/.minecraft-pi/games/com.mojang/minecraftWorlds/world/entities.dat", entitiesParser.getEntities(), entitiesParser.getTileEntities());
+        // ArrayList<PiEntity> entities = entitiesParser.getEntities();
+        // entities.add(hackedInEntity);
+
+        // entitiesParser.assemble("/home/bjtmastermind/.var/app/com.thebrokenrail.MCPIReborn/.minecraft-pi/games/com.mojang/minecraftWorlds/world/entities.dat", entitiesParser.getEntities(), entitiesParser.getTileEntities());
     }
 }
